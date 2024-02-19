55a4dca9552c3d60f89ee842e594f2062f0f0d98
Juan Fiakosky <jfiakosky@hotmail.com>
2024-02-19 12:40:23 -0300

    first commit

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers'; 
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducers";
 
-const store = createStore(rootReducer);
+const store = configureStore({
+	reducer: rootReducer,
+});
 
 export default function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+	return <Provider store={store}>{children}</Provider>;
 }
